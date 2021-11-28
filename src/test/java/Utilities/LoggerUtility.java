f84edb8ce6c20d867781cd49ee05d9ec38b016de
Vickyyamini <Vickyyamini1995@gmail.com>
2021-11-28 12:42:08 +0530

    jenkins Setup

--- src/test/java/Utilities/LoggerUtility.java
+++ src/test/java/Utilities/LoggerUtility.java
@@ -1,5 +1,8 @@
 package Utilities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -8,7 +11,12 @@ import Pages.Constant;
 
 public class LoggerUtility {
 
-	
+	 static{
+	        
+	        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+	        System.setProperty("current.date.time", dateFormat.format(new Date()));
+	    }
+
 	public static  void LogMessage(String ClassName, String Message) {
 		Logger logger= Logger.getLogger(ClassName);
 		BasicConfigurator.configure();
