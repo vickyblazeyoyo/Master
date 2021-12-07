54a67445843ded0d7d23436e6bf6091e070ef46b
Vickyyamini <Vickyyamini1995@gmail.com>
2021-12-07 13:42:44 +0530

    Frameworkcheanges

--- src/test/java/Pages/Constant.java
+++ src/test/java/Pages/Constant.java
@@ -4,12 +4,14 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.WebDriver;
+import org.springframework.util.LinkedCaseInsensitiveMap;
 
 public class Constant {
 public static Scenario scenario;
 public 	static WebDriver driver=null;
 public static LinkedHashMap<String, String> Testdata=new LinkedHashMap<String, String>();
 public static LinkedHashMap<String, String> Failiue_Scenarios_Features=new LinkedHashMap<String, String>();
+public static Map<String,String> Api_Testdata=new LinkedCaseInsensitiveMap<String>();
 public static String Scenario=null,SplitedFeature;
 public static LinkedHashMap<String, String[]> multipleDataSetup=new LinkedHashMap<String, String[]>(); ;
 public static final String Acct_SSID= "AC2d5c48e9195f539cad6ffb46247c88ed";
---
+++ target/test-classes/API_Testing.feature
@@ -0,0 +1,12 @@
+
+@IBSSB
+Feature: IBS Safe Deposit Box APIs Scenarios
+
+  @Test
+  Scenario: IBS Safe Deposit Box API - Set up a box or range of identical boxes
+   Given User Login to Emicalculator Site for Webtable Handling
+When User read year based data
+Then User read month based data
+ 
+
+  
